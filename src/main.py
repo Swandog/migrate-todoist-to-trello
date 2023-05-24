92f849b254057212a2e1eca89ccd9631011d150b
Ken Swanson <ken.swanson@redpoll.ai>
2023-05-24 16:00:53 -0500

    Process sections

--- src/main.py
+++ src/main.py
@@ -8,8 +8,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input", type=pathlib.Path)
 args = parser.parse_args()
 
+def create_list(name: str):
+    print(f"Creating list ({name})")
+
+current_list = None
 with open(args.input, newline='', encoding="utf-8-sig") as csvfile:
     csvreader = csv.DictReader(csvfile)
 
-    for row in csvreader:
-        print(row)
+    for entry in csvreader:
+        if not entry["TYPE"]:
+            continue
+
+        if entry["TYPE"] == "section":
+            current_list = entry["CONTENT"]
+            create_list(current_list)
