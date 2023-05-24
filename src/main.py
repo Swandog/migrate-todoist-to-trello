3940a2fd58990d1cbb18750670316b3d77d26ee6
Ken Swanson <ken.swanson@redpoll.ai>
2023-05-24 15:25:15 -0500

    Read the CSV

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 
+import argparse
+import pathlib
+import csv
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", type=pathlib.Path)
+args = parser.parse_args()
+
+with open(args.input, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+    for row in csvreader:
+        print(', '.join(row))
