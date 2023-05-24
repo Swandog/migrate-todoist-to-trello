4d5ca24eba5e05d0dcf178c6fa147ea19020da48
Ken Swanson <ken.swanson@redpoll.ai>
2023-05-24 16:15:34 -0500

    Pass off all entries to the appropriate (dummy) function

--- src/main.py
+++ src/main.py
@@ -8,10 +8,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input", type=pathlib.Path)
 args = parser.parse_args()
 
-def create_list(name: str):
+def create_list(name: str) -> str:
     print(f"Creating list ({name})")
+    return name
+
+def create_card(list_identifier: str, title: str) -> str:
+    print(f"\tCreating card in list ({list_identifier}) with title ({title})")
+    return title
+
+def add_checklist_item_to_card(card_identifier: str, checklist_text: str):
+    print(f"\t\tAdding checklist item to card ({card_identifier}) with text ({checklist_text})")
 
 current_list = None
+current_card = None
 with open(args.input, newline='', encoding="utf-8-sig") as csvfile:
     csvreader = csv.DictReader(csvfile)
 
@@ -20,5 +29,18 @@ with open(args.input, newline='', encoding="utf-8-sig") as csvfile:
             continue
 
         if entry["TYPE"] == "section":
-            current_list = entry["CONTENT"]
-            create_list(current_list)
+            current_list = create_list(entry["CONTENT"])
+        elif entry["TYPE"] == "task":
+            if current_list is None:
+                raise ValueError()
+
+            indent = int(entry["INDENT"])
+            if indent == 1:
+                current_card = create_card(current_list, entry["CONTENT"])
+            elif indent == 2:
+                add_checklist_item_to_card(current_card, entry["CONTENT"])
+            else:
+                raise ValueError(f"Undefined behavior for task {entry}")
+
+        else:
+            print(f"Unknown record: {entry}")
